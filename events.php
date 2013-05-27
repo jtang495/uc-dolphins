2a83cdfd01e44ebda3144d49c592347d81a98fba
Yang Yang <y4n9squared@gmail.com>
2013-05-27 12:22:34 -0700

    updated meta title for events

--- events.php
+++ events.php
@@ -2,10 +2,8 @@
 <html lang="en">
    <head>
       <meta charset="utf-8">
-      <title>Upper County Dolphins - Information</title>
+      <title>Upper County Dolphins - Events</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta name="description" content="">
-      <meta name="author" content="">
 
       <!-- Le styles -->
       <link href="css/bootstrap.min.css" rel="stylesheet">
