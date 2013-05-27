6a256cdf80ece2c876e80a17915c5cec3bfe741e
Yang Yang <y4n9squared@gmail.com>
2013-05-26 21:42:30 -0700

    updated coaches

--- coaches.php
+++ coaches.php
@@ -35,20 +35,42 @@
             <div class="marketing">
                   <!-- START THE FEATURETTES -->
                   <div class="featurette">
-                     <img class="featurette-image pull-left" src="img/jen-taylor.jpg">
-                     <h2 class="featurette-heading">Jennifer Taylor. <span class="muted">Head Coach.</span></h2>
+                     <img class="featurette-image pull-left polaroid" src="img/jen-taylor.jpg">
+                     <h2 class="featurette-heading">Jennifer Taylor <span class="muted">| Head Coach.</span></h2>
                      <p class="lead">
                      Jen has been our beloved head coach for 15 years. She graduated from UMD with Master degrees in school counseling. She currently works for the MCPS as a guidance counselor at Wootton High School. Jen was an "Outstanding" member of the Westleigh, Wootton High School, RMSC and the University of Maryland swim teams.
                      </p>
                   </div>
                   <hr class="featurette-divider">
                   <div class="featurette">
-                     <img class="featurette-image pull-right" src="img/shannon-king.jpg">
-                     <h2 class="featurette-heading">Shannon King. <span class="muted">Assistant Coach.</span></h2>
+                     <img class="featurette-image pull-right polaroid" src="img/shannon-king.jpg">
+                     <h2 class="featurette-heading">Shannon King <span class="muted">| Assistant Coach.</span></h2>
                      <p class="lead">
-                     Shannon started swimming for Upper County when she was 10 years old and continued to be a Dolphin until she graduated from GHS.  Shannon has been teaching swimming lessons for 9 years.  During the winter she is the head coach for the Gaithersburg High School winter swim team.
-
-                     Shannon is now a graduate from the University of Maryland and working full time so we are happy to have her back on the team!
+                     Shannon started swimming for Upper County when she was 10 years old and continued to be a Dolphin until she graduated from GHS.  Shannon has been teaching swimming lessons for 9 years.  During the winter she is the head coach for the Gaithersburg High School winter swim team. Shannon is now a graduate from the University of Maryland and working full time so we are happy to have her back on the team!
+                     </p>
+                  </div>
+                  <hr class="featurette-divider">
+                  <div class="featurette">
+                     <img class="featurette-image pull-left polaroid" src="img/melanie-page.jpg">
+                     <h2 class="featurette-heading">Melanie Page <span class="muted">| Assistant Coach.</span></h2>
+                     <p class="lead">
+                     Melanie is a senior at Salsbury University studying Health Education with a minor in Psychology. Melanie has been a coach for Upper County for the past 4 summers. Prior to coaching Melanie was a swimmer on our team. She began swimming with Upper County when she was 6.
+                     </p>
+                  </div>
+                  <hr class="featurette-divider">
+                  <div class="featurette">
+                     <img class="featurette-image pull-right polaroid" src="">
+                     <h2 class="featurette-heading">Scott Page <span class="muted">| Assistant Coach.</span></h2>
+                     <p class="lead">
+                     Something about Scott Page.
+                     </p>
+                  </div>
+                  <hr class="featurette-divider">
+                  <div class="featurette">
+                     <img class="featurette-image pull-left polaroid" src="">
+                     <h2 class="featurette-heading">Yang Yang <span class="muted">| Assistant Coach.</span></h2>
+                     <p class="lead">
+                     Something about Yang Yang.
                      </p>
                   </div>
                   <hr class="featurette-divider">
