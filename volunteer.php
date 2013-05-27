d0264c407c715c0c6d7afcb8c225d223678694d2
Yang Yang <y4n9squared@gmail.com>
2013-05-27 09:39:41 -0700

    added volunteering page

---
+++ volunteer.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <title>Upper County Dolphins - Volunteer</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+      <!-- Le styles -->
+      <link href="css/bootstrap.min.css" rel="stylesheet">
+      <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+      <link href="css/style.css" rel="stylesheet">
+
+      <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+      <!--[if lt IE 9]>
+         <script src="../assets/js/html5shiv.js"></script>
+    < ![endif]-->
+   </head>
+
+   <body>
+      <!-- Wrap body contents in .wrap to push footer down to bottom of page -->
+      <div id="wrap">
+         <!-- Include navigation bar -->
+         <?php require 'navbar.inc.php' ?>         
+         <div id="push"></div>
+         <!-- Wrap the rest of the page in another container to center all the content. -->
+         <div class="container">
+            <div class="marketing">
+                  <!-- START THE FEATURETTES -->
+                  <div class="featurette">
+                     <img class="featurette-image pull-left" src="../assets/img/examples/browser-icon-firefox.png">
+                     <h2 class="featurette-heading">Volunteering. <span class="muted">We need your help.</span></h2>
+                     <p class="lead">Parent volunteers are critical to the success of the swim season. It takes between 45 to 60 parents to run a swim meet! Many jobs require little to no experience such as timing and selling food at concessions. Other jobs such as referee, starter, and stroke/turn judge require certification. Although most jobs are suited for adults, some jobs can be performed by older swimmers such as runner, ribbon writer, verifier, and clerk of course.</p>
+                     <div class="alert alert-info">
+                        <strong>Heads up!</strong> The volunteer requirement per family is 5 obligations. Failure to fulfill this mandatory obligation will result a forfeiture of your child's priviledge to participate in meets.
+                     </div>
+                     <button class="btn btn-large btn-primary" type="button">Sign up now! <i class="icon-user"></i></button>
+                  </div>
+                  <hr class="featurette-divider">
+                  <div class="featurette">
+                     <h2 class="featurette-heading">Community Service. <span class="muted">Earn SSL hours.</span></h2>
+                     <p class="lead">Middle and high school swimmers and their siblings can earn SSL hours by volunteering at meets. Our volunteer coordinator can keep track of your swimmers' SSL hours and provide a SSL form with their accumulated hours at the end of the season.</p>
+                 </div>
+                  <hr class="featurette-divider">
+                  <!-- /END THE FEATURETTES -->
+            </div><!-- /.marketing -->
+         </div><!-- /.container -->
+         <div id="push"></div>
+      </div><!-- /.wrap -->
+      <?php require 'footer.inc.php' ?>
+   </body>
+</html>
