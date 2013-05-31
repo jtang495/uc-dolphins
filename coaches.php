c7ed2dbc125130c8cc20548adbedf8e6c8c0dcf7
Yang Yang <y4n9squared@gmail.com>
2013-05-31 08:28:41 -0700

    fixed text alignment on coaches page

--- coaches.php
+++ coaches.php
@@ -1,78 +1,61 @@
-<!DOCTYPE html>
-<html lang="en">
-   <head>
-      <meta charset="utf-8">
-      <title>Upper County Dolphins - Coaches</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta name="description" content="">
-      <meta name="author" content="">
-
-      <!-- Le styles -->
-      <link href="css/bootstrap.min.css" rel="stylesheet">
-      <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
-      <link href="css/style.css" rel="stylesheet">
-
-      <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-      <!--[if lt IE 9]>
-         <script src="../assets/js/html5shiv.js"></script>
-    < ![endif]-->
-   </head>
-
-   <body>
-      <!-- Wrap body contents in .wrap to push footer down to bottom of page -->
-      <div id="wrap">
-         <!-- Include nagivation bar -->
-         <?php require 'navbar.inc.php' ?>
-         <div id="push"></div>
-         <!-- Wrap the rest of the page in another container to center all the content. -->
-         <div class="container">
-            <div class="marketing">
-                  <!-- START THE FEATURETTES -->
-                  <div class="featurette">
-                     <img class="featurette-image pull-left img-polaroid" src="img/jen-taylor.jpg">
-                     <h2 class="featurette-heading">Jennifer Taylor<span class="muted"> | Head Coach</span></h2>
-                     <p class="lead">
-                     Jen has been our beloved head coach for 15 years. She graduated from UMD with Master degrees in school counseling. She currently works for the MCPS as a guidance counselor at Wootton High School. Jen was an "Outstanding" member of the Westleigh, Wootton High School, RMSC and the University of Maryland swim teams.
-                     </p>
-                  </div>
-                  <hr class="featurette-divider">
-                  <div class="featurette">
-                     <img class="featurette-image pull-right img-polaroid" src="img/shannon-king.jpg">
-                     <h2 class="featurette-heading">Shannon King<span class="muted"> | Assistant Coach</span></h2>
-                     <p class="lead">
-                     Shannon started swimming for Upper County when she was 10 years old and continued to be a Dolphin until she graduated from GHS. Shannon has been teaching swimming lessons for 9 years. During the winter she is the head coach for the Gaithersburg High School winter swim team. Shannon is now a graduate from the University of Maryland and working full time so we are happy to have her back on the team!
-                     </p>
-                  </div>
-                  <hr class="featurette-divider">
-                  <div class="featurette">
-                     <img class="featurette-image pull-left img-polaroid" src="img/melanie-page.jpg">
-                     <h2 class="featurette-heading">Melanie Page<span class="muted"> | Assistant Coach</span></h2>
-                     <p class="lead">
-                     Melanie is a senior at Salsbury University studying Health Education with a minor in Psychology. Melanie has been a coach for Upper County for the past 4 summers. Prior to coaching Melanie was a swimmer on our team. She began swimming with Upper County when she was 6.
-                     </p>
-                  </div>
-                  <hr class="featurette-divider">
-                  <div class="featurette">
-                     <img class="featurette-image pull-right polaroid" src="">
-                     <h2 class="featurette-heading">Scott Page<span class="muted"> | Assistant Coach</span></h2>
-                     <p class="lead">
-                     Something about Scott Page.
-                     </p>
-                  </div>
-                  <hr class="featurette-divider">
-                  <div class="featurette">
-                     <img class="featurette-image pull-left polaroid" src="">
-                     <h2 class="featurette-heading">Yang Yang<span class="muted"> | Assistant Coach</span></h2>
-                     <p class="lead">
-                     Yang joined Upper County when he was 13 years old. He was an assistant coach on the team during his senior year in high school in 2008. Yang recently graduated from MIT with a master of engineering degree and will be working for Blizzard Entertainment starting in the fall.
-                     </p>
-                  </div>
-                  <hr class="featurette-divider">
-                  <!-- /END THE FEATURETTES -->
-            </div><!-- /.marketing -->
-         </div><!-- /.container -->
-         <div id="push"></div>
-      </div><!-- /.wrap -->
-      <?php require 'footer.inc.php' ?>
-   </body>
-</html>
+<?php require 'head.inc.php' ?>
+<style>
+  .featurette.bio-left {
+    text-align: left;
+    overflow: hidden;
+  }
+  .featurette-image.bio {
+    margin: 20px;
+    overflow: hidden;
+  }
+  .featurette.bio-right {
+    text-align: right;
+  }
+</style>
+<title>Upper County Dolphins - Coaches</title>
+<div id="wrap">
+  <!-- Include nagivation bar -->
+  <?php require 'navbar.inc.php' ?>
+  <div id="push"></div>
+  <!-- Wrap the rest of the page in another container to center all the content. -->
+  <div class="container">
+    <div class="marketing">
+      <div class="featurette bio-left">
+        <img class="featurette-image pull-left img-polaroid bio" src="img/jen-taylor.jpg">
+        <h2 class="featurette-heading">Jennifer Taylor<span class="muted"> | Head Coach</span></h2>
+        <p class="lead bio-left">
+          Jen has been our beloved head coach for 15 years. She graduated from UMD with Master degrees in school counseling. She currently works for the MCPS as a guidance counselor at Wootton High School. Jen was an "Outstanding" member of the Westleigh, Wootton High School, RMSC and the University of Maryland swim teams.
+      </div>
+      <hr class="featurette-divider">
+      <div class="featurette bio-right">
+        <img class="featurette-image pull-right img-polaroid bio" src="img/shannon-king.jpg">
+        <h2 class="featurette-heading">Shannon King<span class="muted"> | Assistant Coach</span></h2>
+        <p class="lead">
+          Shannon started swimming for Upper County when she was 10 years old and continued to be a Dolphin until she graduated from GHS. Shannon has been teaching swimming lessons for 9 years. During the winter she is the head coach for the Gaithersburg High School winter swim team. Shannon is now a graduate from the University of Maryland and working full time so we are happy to have her back on the team!
+      </div>
+      <hr class="featurette-divider">
+      <div class="featurette bio-left">
+        <img class="featurette-image pull-left img-polaroid bio" src="img/melanie-page.jpg">
+        <h2 class="featurette-heading">Melanie Page<span class="muted"> | Assistant Coach</span></h2>
+        <p class="lead">
+          Melanie is a senior at Salsbury University studying Health Education with a minor in Psychology. Melanie has been a coach for Upper County for the past 4 summers. Prior to coaching Melanie was a swimmer on our team. She began swimming with Upper County when she was 6.
+      </div>
+      <hr class="featurette-divider">
+      <div class="featurette bio-right">
+        <img class="featurette-image pull-right polaroid bio" src="">
+        <h2 class="featurette-heading">Scott Page<span class="muted"> | Assistant Coach</span></h2>
+        <p class="lead">
+          Something about Scott Page.
+      </div>
+      <hr class="featurette-divider">
+      <div class="featurette bio-left">
+        <img class="featurette-image pull-left polaroid bio" src="">
+        <h2 class="featurette-heading">Yang Yang<span class="muted"> | Assistant Coach</span></h2>
+        <p class="lead">
+          Yang joined Upper County when he was 13 years old. He was an assistant coach on the team during his senior year in high school in 2008. Yang recently graduated from MIT with a master of engineering degree and will be working for Blizzard Entertainment starting in the fall.
+      </div><!-- /.featurettes -->
+    </div><!-- /.marketing -->
+  </div><!-- /.container -->
+  <div id="push"></div>
+</div><!-- /.wrap -->
+<?php require 'footer.inc.php' ?>
