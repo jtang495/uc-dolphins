b74338dbc4cfc1260c50fdaac01d4ae2a96d1963
Yang Yang <y4n9squared@gmail.com>
2013-05-31 08:42:49 -0700

    refactored and fixed style issues

--- records.php
+++ records.php
@@ -1,279 +1,229 @@
 <!DOCTYPE html>
-<html lang="en">
-   <head>
-      <meta charset="utf-8">
-      <title>Upper County Dolphins - Records</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-      <!-- Le styles -->
-      <link href="css/bootstrap.min.css" rel="stylesheet">
-      <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
-      <link href="css/style.css" rel="stylesheet">
-
-      <style>
-         #team-records td {
-            text-align: center;
-         }
-         #team-records th {
-            text-align: center;
-         }
-      </style>
-
-      <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-      <!--[if lt IE 9]>
-         <script src="../assets/js/html5shiv.js"></script>
-    < ![endif]-->
-   </head>
-
-   <body>
-      <!-- Wrap body contents in .wrap to push footer down to bottom of page -->
-      <div id="wrap">
-         <!-- Include navigation bar -->
-         <?php require 'navbar.inc.php' ?>         
-         <div id="push"></div>
-         <!-- Wrap the rest of the page in another container to center all the content. -->
-         <div class="container">
-            <div class="marketing">
-               <!-- START THE FEATURETTES -->
-               <div class="featurette">
-                  <h2 class="featurette-heading">Upper County Records <span class="muted">| Make your mark in history.</span></h2>
-                  <div id="push"></div>
-                  <p>
-                     <div class="btn-group" data-toggle="buttons-radio">
-                        <button type="button" class="btn active">Team</button>
-                        <button type="button" class="btn">Pool</button>
-                        <button type="button" class="btn">Relay</button>
-                     </div>
-                  </p>
-                  <table id="team-records" class="table table-striped table-hover">
-                     <tr>
-                        <th colspan="3">Boys</th>
-                        <th></th>
-                        <th colspan="3">Girls</th>
-                     </tr>
-                     <tr>
-                        <th>Event Number</th>
-                        <th>Name</th>
-                        <th>Time</th>
-                        <th>Event</th>
-                        <th>Time</th>
-                        <th>Name</th>
-                        <th>Event Number</th>
-                     </tr>
-                     <tr>
-                        <td>1</td>
-                        <td>John Doe</td>
-                        <td>1:00.00 <span class="label label-info"> new </span></td>
-                        <td>12 & Under 100m IM</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>2</td>
-                     </tr>
-                     <tr>
-                        <td>5</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>8 & Under 25m Free</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>6</td>
-                     </tr>
-                     <tr>
-                        <td>7</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>9-10 50m Free</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>8</td>
-                     </tr>
-                     <tr>
-                        <td>9</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>13-14 50m Free</td>
-                        <td>1:10.00 <span class="label label-warning"> MCSL </span></td>
-                        <td>Jane Done</td>
-                        <td>10</td>
-                     </tr>
-                     <tr>
-                        <td>11</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>11-12 50m Free</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>12</td>
-                     </tr>
-                     <tr>
-                        <td>13</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>15-18 100m Free</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>14</td>
-                     </tr>
-                     <tr>
-                        <td>15</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>13-14 100m IM</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>16</td>
-                     </tr>
-                     <tr>
-                        <td>17</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>8 & Under 25m Back</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>18</td>
-                     </tr>
-                     <tr>
-                        <td>19</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>9-10 25m Back</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>20</td>
-                     </tr>
-                     <tr>
-                        <td>21</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>15-18 100m Back</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>22</td>
-                     </tr>
-                     <tr>
-                        <td>23</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>11-12 50m Back</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>24</td>
-                     </tr>
-                     <tr>
-                        <td>25</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>13-14 50m Back</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>26</td>
-                     </tr>
-                     <tr>
-                        <td>27</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>15-18 100m IM</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>28</td>
-                     </tr>
-                     <tr>
-                        <td>29</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>8 & Under 25m Breast</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>30</td>
-                     </tr>
-                     <tr>
-                        <td>31</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>9-10 25m Breast</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>32</td>
-                     </tr>
-                     <tr>
-                        <td>33</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>11-12 50m Breast</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>34</td>
-                     </tr>
-                     <tr>
-                        <td>35</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>13-14 50m Breast</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>36</td>
-                     </tr>
-                     <tr>
-                        <td>37</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>15-18 100m Breast</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>38</td>
-                     </tr>
-                     <tr>
-                        <td>39</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>8 & Under 25m Fly</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>40</td>
-                     </tr>
-                     <tr>
-                        <td>41</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>9-10 25m Fly</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>42</td>
-                     </tr>
-                     <tr>
-                        <td>43</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>11-12 50m Fly</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>44</td>
-                     </tr>
-                     <tr>
-                        <td>45</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>13-14 50m Fly</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>46</td>
-                     </tr>
-                     <tr>
-                        <td>47</td>
-                        <td>John Doe</td>
-                        <td>1:00.00</td>
-                        <td>15-18 50m Fly</td>
-                        <td>1:10.00</td>
-                        <td>Jane Done</td>
-                        <td>48</td>
-                     </tr>
-                  </table>
-               </div>
-               <!-- /END THE FEATURETTES -->
-            </div><!-- /.marketing -->
-         </div><!-- /.container -->
-         <div id="push"></div>
-      </div><!-- /.wrap -->
-      <?php require 'footer.inc.php' ?>
-   </body>
-</html>
+<?php require 'head.inc.php' ?>
+<title>Upper County Dolphins - Records</title>
+<style>
+    #team-records td {
+      text-align: center;
+    }
+    #team-records th {
+      text-align: center;
+    }
+</style>
+<div id="wrap">
+  <!-- Include navigation bar -->
+  <?php require 'navbar.inc.php' ?>         
+  <div id="push"></div>
+  <div class="container">
+    <div class="marketing">
+      <div class="featurette">
+        <h2 class="featurette-heading">Upper County Records <span class="muted">| Make your mark in history.</span></h2>
+        <div id="push"></div>
+        <p>
+          <div class="btn-group" data-toggle="buttons-radio">
+            <button type="button" class="btn active">Team</button>
+            <button type="button" class="btn">Pool</button>
+            <button type="button" class="btn">Relay</button>
+          </div>
+        <table id="team-records" class="table table-striped table-hover">
+          <tr>
+            <th colspan="3">Boys
+            <th>
+            <th colspan="3">Girls
+          <tr>
+            <th>Event Number
+            <th>Name
+            <th>Time
+            <th>Event
+            <th>Time
+            <th>Name
+            <th>Event Number
+          <tr>
+            <td>1
+            <td>John Doe
+            <td>1:00.00 <span class="label label-info"> new </span>
+            <td>12 & Under 100m IM
+            <td>1:10.00
+            <td>Jane Done
+            <td>2
+          <tr>
+            <td>5
+            <td>John Doe
+            <td>1:00.00
+            <td>8 & Under 25m Free
+            <td>1:10.00
+            <td>Jane Done
+            <td>6
+          <tr>
+            <td>7
+            <td>John Doe
+            <td>1:00.00
+            <td>9-10 50m Free
+            <td>1:10.00
+            <td>Jane Done
+            <td>8
+          <tr>
+            <td>9
+            <td>John Doe
+            <td>1:00.00
+            <td>13-14 50m Free
+            <td>1:10.00 <span class="label label-warning"> MCSL </span>
+            <td>Jane Done
+            <td>10
+          <tr>
+            <td>11
+            <td>John Doe
+            <td>1:00.00
+            <td>11-12 50m Free
+            <td>1:10.00
+            <td>Jane Done
+            <td>12
+          <tr>
+            <td>13
+            <td>John Doe
+            <td>1:00.00
+            <td>15-18 100m Free
+            <td>1:10.00
+            <td>Jane Done
+            <td>14
+          <tr>
+            <td>15
+            <td>John Doe
+            <td>1:00.00
+            <td>13-14 100m IM
+            <td>1:10.00
+            <td>Jane Done
+          <tr>
+            <td>17
+            <td>John Doe
+            <td>1:00.00
+            <td>8 & Under 25m Back
+            <td>1:10.00
+            <td>Jane Done
+            <td>18
+          <tr>
+            <td>19
+            <td>John Doe
+            <td>1:00.00
+            <td>9-10 25m Back
+            <td>1:10.00
+            <td>Jane Done
+            <td>20
+          <tr>
+            <td>21
+            <td>John Doe
+            <td>1:00.00
+            <td>15-18 100m Back
+            <td>1:10.00
+            <td>Jane Done
+            <td>22
+          <tr>
+            <td>23
+            <td>John Doe
+            <td>1:00.00
+            <td>11-12 50m Back
+            <td>1:10.00
+            <td>Jane Done
+            <td>24
+          <tr>
+            <td>25
+            <td>John Doe
+            <td>1:00.00
+            <td>13-14 50m Back
+            <td>1:10.00
+            <td>Jane Done
+            <td>26
+          <tr>
+            <td>27
+            <td>John Doe
+            <td>1:00.00
+            <td>15-18 100m IM
+            <td>1:10.00
+            <td>Jane Done
+            <td>28
+          <tr>
+            <td>29
+            <td>John Doe
+            <td>1:00.00
+            <td>8 & Under 25m Breast
+            <td>1:10.00
+            <td>Jane Done
+            <td>30
+          <tr>
+            <td>31
+            <td>John Doe
+            <td>1:00.00
+            <td>9-10 25m Breast
+            <td>1:10.00
+            <td>Jane Done
+            <td>32
+          <tr>
+            <td>33
+            <td>John Doe
+            <td>1:00.00
+            <td>11-12 50m Breast
+            <td>1:10.00
+            <td>Jane Done
+            <td>34
+          <tr>
+            <td>35
+            <td>John Doe
+            <td>1:00.00
+            <td>13-14 50m Breast
+            <td>1:10.00
+            <td>Jane Done
+            <td>36
+          <tr>
+            <td>37
+            <td>John Doe
+            <td>1:00.00
+            <td>15-18 100m Breast
+            <td>1:10.00
+            <td>Jane Done
+            <td>38
+          <tr>
+            <td>39
+            <td>John Doe
+            <td>1:00.00
+            <td>8 & Under 25m Fly
+            <td>1:10.00
+            <td>Jane Done
+            <td>40
+          <tr>
+            <td>41
+            <td>John Doe
+            <td>1:00.00
+            <td>9-10 25m Fly
+            <td>1:10.00
+            <td>Jane Done
+            <td>42
+          <tr>
+            <td>43
+            <td>John Doe
+            <td>1:00.00
+            <td>11-12 50m Fly
+            <td>1:10.00
+            <td>Jane Done
+            <td>44
+          <tr>
+            <td>45
+            <td>John Doe
+            <td>1:00.00
+            <td>13-14 50m Fly
+            <td>1:10.00
+            <td>Jane Done
+            <td>46
+          <tr>
+            <td>47
+            <td>John Doe
+            <td>1:00.00
+            <td>15-18 50m Fly
+            <td>1:10.00
+            <td>Jane Done
+            <td>48
+        </table>
+      </div> <!-- /.featurette -->
+    </div><!-- /.marketing -->
+  </div><!-- /.container -->
+  <div id="push"></div>
+</div><!-- /.wrap -->
+<?php require 'footer.inc.php' ?>
