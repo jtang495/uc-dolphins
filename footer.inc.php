b743fa7346d9ecc83253564942e56b10299e5cf5
Yang Yang <y4n9squared@gmail.com>
2013-06-02 19:29:14 -0700

    added pool address to footer

--- footer.inc.php
+++ footer.inc.php
@@ -2,7 +2,7 @@
 <div id="footer">
    <div class="container">
       <p class="pull-right"><a href="#">Back to top</a></p>
-      <p>&copy; 2013 Upper County Dolphins. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      <p>&copy; 2013 Upper County Dolphins. &middot; 8211 Emory Grove Rd. Gaithersburg MD 20878</p>
    </div>
 </div>
 
