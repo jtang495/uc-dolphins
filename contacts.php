83ba88252e26e7f5812a05db26b4056729e31a8e
Yang Yang <y4n9squared@gmail.com>
2013-06-02 19:10:26 -0700

    updated contacts

--- contacts.php
+++ contacts.php
@@ -10,88 +10,97 @@
     <div class="marketing">
       <div class="featurette">
         <h2 class="featurette-heading">Contacts | <span class="muted">Get in touch.</span></h2>
-          <div class="row-fluid">
-            <div class="span4">
-              <h3>A Rep</h3>
-              <p class="lead">Lilly Brotons</br>
-              <a href="mailto:lbsbailey@gmail.com">lbsbailey@gmail.com</a></br>
-              (240) 286-4841
-            </div>
-            <div class="span4">
-              <h3>B Rep</h3>
-              <p class="lead">Jackie Suzich</br>
-              <a href="mailto:jsuzich@comcast.net">jsuzich@comcast.net</a><br>
-              (301) 990-6088
-            </div>
-            <div class="span4">
-              <h3>B Rep</h3>
-              <p class="lead">Lori Littrell</br>
-              <a href="mailto:llitrell64@gmail.com">llitrell64@gmail.com</a></br>
-              (301) 641-0504
-            </div>
+        <div class="row-fluid">
+          <div class="span4">
+            <h3>Team Rep</h3>
+            <p class="lead">Lilly Brotons</br>
+            <a href="mailto:lbsbailey@gmail.com">lbsbailey@gmail.com</a></br>
+            (240) 286-4841
           </div>
-          <div class="row-fluid">
-            <div class="span4">
-              <h3>Treasurer</h3>
-              <p class="lead">Melanie O'Leary</br>
-              <a href="mailto:oleary25@verizon.net">oleary25@verizon.net</a></br>
-              (301) 253-2631
-            </div>
-            <div class="span4">
-              <h3>Volunteer Coordinator</h3>
-              <p class="lead">Root Teklemarian</br>
-              <a href="mailto:roottekle@gmail.com">root.teckle@gmail.com</a></br>
-              (240) 533-6466
-            </div>
-            <div class="span4">
-              <h3>A Concessions</h3>
-              <p class="lead">Vi Cu</br>
-              <a href="mailto:triducvi@gmail.com">triducvi@gmail.com</a></br>
-              (301) 339-4369
-            </div>
+          <div class="span4">
+            <h3>A Rep</h3>
+            <p class="lead">Position Open!</br>
           </div>
-          <div class="row-fluid">
-            <div class="span4">
-              <h3>B Concessions</h3>
-              <p class="lead">Jessica Jones</br>
-              <a href="mailto:jessicamorganjones@gmail.com">jessicamorganjones@gmail.com</a></br>
-              (703) 901-5280
-            </div>
-            <div class="span4">
-              <h3>Spirit Wear</h3>
-              <p class="lead">Linda Solomich</br>
-              <a href="mailto:lsolomich@comneticsinc.com">lsolomich@comneticsinc.com</a></br>
-              (301) 335-5456
-            </div>
-            <div class="span4">
-              <h3>General Information</h3>
-              <p class="lead">Lauren Mannella</br>
-              <a href="mailto:laurenmannella@yahoo.com">laurenmannella@yahoo.com</a></br>
-            </div>
+          <div class="span4">
+            <h3>B Rep</h3>
+            <p class="lead">Jackie Suzich</br>
+            <a href="mailto:jsuzich@comcast.net">jsuzich@comcast.net</a><br>
+            (301) 990-6088
           </div>
-          <div class="row-fluid">
-            <div class="span4">
-              <h3>New Parent Contact, Pep Rally & Secret Swimmer</h3>
-              <p class="lead">Laura Ado</br>
-              <a href="mailto:lauraado@hotmail.com">lauraado@hotmail.com</a></br>
-            </div>
-            <div class="span4">
-              <h3>Officials Coordinator</h3>
-              <p class="lead">Dan Solomich</br>
-              <a href="mailto:dsolomich@comneticsinc.com">dsolomich@comneticsinc.com</a></br>
-            </div>
-            <div class="span4">
-              <h3>Automation</h3>
-              <p class="lead">Brian Hedquist</br>
-              <a href="mailto:bfhedquist@earthlink.com">bfhedquist@earthlink.com</a></br>
-            </div>
+        </div>
+        <div class="row-fluid">
+          <div class="span4">
+            <h3>B Rep</h3>
+            <p class="lead">Lori Littrell</br>
+            <a href="mailto:llitrell64@gmail.com">llitrell64@gmail.com</a></br>
+            (301) 641-0504
           </div>
-          <div class="row-fluid">
-            <div class="span4">
-              <h3>Automation</h3>
-              <p class="lead">Carolyn Golding</br>
-              <a href="mailto:carolynsky41@aol.com">carolynsky41@aol.com</a></br>
-            </div>
+          <div class="span4">
+            <h3>Automation</h3>
+            <p class="lead">Brian Hedquist</br>
+            <a href="mailto:bfhedquist@earthlink.net">bfhedquist@earthlink.net</a></br>
+            (240) 272-7293
+          </div>
+          <div class="span4">
+            <h3>Automation</h3>
+            <p class="lead">Carolyn Golding</br>
+            <a href="mailto:carolynsky41@aol.com">carolynsky41@aol.com</a></br>
+            (301) 814-1016
+          </div>
+        </div>
+        <div class="row-fluid">
+          <div class="span4">
+            <h3>A Concessions</h3>
+            <p class="lead">Vi Cu</br>
+            <a href="mailto:triducvi@gmail.com">triducvi@gmail.com</a></br>
+            (301) 339-4369
+          </div>
+          <div class="span4">
+            <h3>B Concessions</h3>
+            <p class="lead">Jessica Jones</br>
+            <a href="mailto:jessicamorganjones@gmail.com">jessicamorganjones@gmail.com</a></br>
+            (703) 901-5280
+          </div>
+          <div class="span4">
+            <h3>General Information</h3>
+            <p class="lead">Lauren Mannella</br>
+            <a href="mailto:laurenmannella@yahoo.com">laurenmannella@yahoo.com</a></br>
+            (301) 529-9853
+          </div>
+        </div>
+        <div class="row-fluid">
+          <div class="span4">
+            <h3>New Parent Contact</h3>
+            <p class="lead">Laura Ado</br>
+            <a href="mailto:lauraado@hotmail.com">lauraado@hotmail.com</a></br>
+            (301) 370-5214
+          </div>
+          <div class="span4">
+            <h3>Officials Coordinator</h3>
+            <p class="lead">Dan Solomich</br>
+            <a href="mailto:dsolomich@comneticsinc.com">dsolomich@comneticsinc.com</a></br>
+            (301) 910-8681
+          </div>
+          <div class="span4">
+            <h3>Spirit Wear</h3>
+            <p class="lead">Linda Solomich</br>
+            <a href="mailto:lsolomich@comneticsinc.com">lsolomich@comneticsinc.com</a></br>
+            (301) 335-5456
+          </div>
+        </div>
+        <div class="row-fluid">
+          <div class="span4">
+            <h3>Treasurer</h3>
+            <p class="lead">Melanie O'Leary</br>
+            <a href="mailto:oleary25@verizon.net">oleary25@verizon.net</a></br>
+            (301) 253-2631
+          </div>
+          <div class="span4">
+            <h3>Volunteer Coordinator</h3>
+            <p class="lead">Root Owusu</br>
+            <a href="mailto:roottekle@gmail.com">root.tekle@gmail.com</a></br>
+            (240) 533-6466
+          </div>
         </div><!-- /.fluid -->
       </div><!-- /.featurette -->
     </div><!-- /.marketing -->
