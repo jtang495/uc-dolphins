87254d9d7afcc34270da6442d6e1fa73dffbd78f
Yang Yang <y4n9squared@gmail.com>
2013-06-06 11:56:28 -0700

    added featurette-video container

--- events.php
+++ events.php
@@ -9,21 +9,6 @@
   <div class="container">
     <div class="marketing">
       <div class="featurette">
-        <img class="featurette-image pull-left" src="../assets/img/examples/browser-icon-firefox.png">
-        <h2 class="featurette-heading">Pep Rally and Caravan Fun. <span class="muted">Show some spirit</span></h2>
-        <p class="lead">
-          Pep rallies are a favorite activity each summer for all the swimmers.  Each Thursday before a home A meet the team will be making posters and decorating the pool for the meet!  Pep rally’s are for all Upper County Dolphin team members regardless of whether or not you are swimming in the meets. Come and be a part of the team spirit!!<br/>
-        Another team tradition is to travel together in a "caravan of cars" to away morning meets.  We will meet at Upper County pool where we can decorate the cars and the children!
-      </div>
-      <hr class="featurette-divider">
-      <div class="featurette">
-        <img class="featurette-image pull-right" src="img/six-flags.jpg">
-        <h2 class="featurette-heading">MCSL Six Flags Day. <span class="muted">Exclusive access</span></h2>
-        <p class="lead">
-          MCSL enjoys exclusive use of the Hurricane Harbor Water Park on Monday July 15, 2013 from 9:30-11:00 am. Tickets are approximately $25. Pre-order parking and meal vouchers are available. More information coming soon...
-      </div>
-      <hr class="featurette-divider">
-      <div class="featurette">
         <img class="featurette-image img-polaroid pull-left" src="img/thumb-team-photo.jpg">
         <h2 class="featurette-heading">Pasta Pot Luck and Pep Rally</h2>
         <p class="lead">
@@ -38,8 +23,20 @@
       </div>
       <hr class="featurette-divider">
       <div class="featurette">
-        <h2 class="featurette-heading">Team Movie Outing. <span class="muted">Monsters University<span></h2>
+        <img class="featurette-image pull-right" src="img/six-flags.jpg">
+        <h2 class="featurette-heading">MCSL Six Flags Day. <span class="muted">Exclusive access</span></h2>
+        <p class="lead">
+          MCSL enjoys exclusive use of the Hurricane Harbor Water Park on Monday July 15, 2013 from 9:30-11:00 am. Tickets are approximately $25. Pre-order parking and meal vouchers are available. More information coming soon...
+        <p class="lead">
+          <strong>Date:</strong> Monday, July 15</br>
+          <strong>Where:</strong> Six Flags America
+      </div>
+      <hr class="featurette-divider">
+      <div class="featurette">
+        <div class="featurette-video pull-left">
         <iframe width="560" height="315" src="http://www.youtube.com/embed/xBzPioph8CI?rel=0" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <h2 class="featurette-heading">Team Movie Outing. <span class="muted">Monsters University<span></h2>
         <p class="lead">
           Exact time will be announced when showtimes are released!
         <p class="lead">
@@ -49,7 +46,7 @@
       </div>
       <hr class="featurette-divider">
       <div class="featurette">
-        <img class="featurette-image img-polaroid pull-left" src="img/thumb-pajama-night.jpg" style="width: 40%;">
+        <img class="featurette-image img-polaroid pull-right" src="img/thumb-pajama-night.jpg" style="width: 40%;">
         <h2 class="featurette-heading">Pajama Movie Night & Secret Swimmer</h2>
         <p class="lead">
           Start shopping, creating, plotting, and planning! Secret swimmer bins will be out first thing each morning. They are three blue crates containers with folders for swimmers labeled alphabetically. Just insert your gift into the swimmers folder, or their swim bag, or their car when they aren't nearby.
