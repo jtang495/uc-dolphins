34788e4bb397cac9f0b31e9cc6fcb89c806b57ea
Yang Yang <y4n9squared@gmail.com>
2013-06-07 14:59:28 -0700

    updated pool recrods

--- pool-records.inc.php
+++ pool-records.inc.php
@@ -16,9 +16,9 @@
     <th>Event Number
   <tr>
     <td>1
-    <td>Jesse Cunningham
-    <td>Aug 03, 2003
-    <td>1:07.21
+    <td>Devin Truong (RV)
+    <td>Jul 23, 2011
+    <td>1:06.17
     <td>12 & Under 100m IM
     <td>1:09.84
     <td>Jun 14, 2008
@@ -26,9 +26,9 @@
     <td>2
   <tr>
     <td>5
-    <td>Leo Seen
-    <td>Jul 03, 2012
-    <td>16.13
+    <td>Timmy Ellett (TW)
+    <td>Jun 20, 2009
+    <td>15.38
     <td>8 & Under 25m Free
     <td>16.47
     <td>Jul 21, 2007
@@ -36,9 +36,9 @@
     <td>6
   <tr>
     <td>7
-    <td>Samuel Tartar
-    <td>Jul 11, 2009
-    <td>31.52
+    <td>Timmy Ellett (TW)
+    <td>Jul 23, 2011
+    <td>30.59
     <td>9-10 50m Free
     <td>30.81
     <td>Jul 23, 2011
@@ -46,9 +46,9 @@
     <td>8
   <tr>
     <td>9
-    <td>Jesse Cunningham
-    <td>Jul 31, 2005
-    <td>25.37
+    <td>Jesse Cunningham (UC)
+    <td>Jun 18, 2005
+    <td>25.78
     <td>13-14 50m Free
     <td>27.76
     <td>Jun 19, 2004
@@ -56,9 +56,9 @@
     <td>10
   <tr>
     <td>11
-    <td>Jesse Cunningham
-    <td>Aug 03, 2003
-    <td>27.63
+    <td>Jesse Cunningham (UC)
+    <td>Jul 19, 2003
+    <td>28.43
     <td>11-12 50m Free
     <td>29.17
     <td>Jun 26, 2010
@@ -66,9 +66,9 @@
     <td>12
   <tr>
     <td>13
-    <td>Jesse Cunningham
+    <td>Sean Stewart (WG)
     <td>Jul 21, 2007
-    <td>53.40
+    <td>53.18
     <td>15-18 100m Free
     <td>59.60
     <td>Jul 23, 2011
@@ -76,9 +76,9 @@
     <td>14
   <tr>
     <td>15
-    <td>Jesse Cunningham
-    <td>Jul 31, 2005
-    <td>1:01.81
+    <td>Jesse Cunningham (UC)
+    <td>Jul 09, 2005
+    <td>1:02.59
     <td>13-14 100m IM
     <td>1:09.12
     <td>Jul 23, 2011
@@ -86,9 +86,9 @@
     <td>16
   <tr>
     <td>17
-    <td>Victor Wang
-    <td>Jul 01, 1999
-    <td>19.96
+    <td>Darius Truong (RV)
+    <td>Jul 09, 2011
+    <td>18.49
     <td>8 & Under 25m Back
     <td>20.17
     <td>Jul 09, 2011
@@ -96,19 +96,19 @@
     <td>20
   <tr>
     <td>19
-    <td>Victor Wang
-    <td>Jul 14, 2001
-    <td>17.56
+    <td>Timmy Ellett (TW)
+    <td>Jul 23, 2011
+    <td>16.81
     <td>9-10 25m Back
-    <td>16.29 <span class="label label-info">MCSL</span>
+    <td>16.29
     <td>Jun 26, 2010
     <td>Emily T Wang (UC)
     <td>22
   <tr>
     <td>21
-    <td>Jesse Cunningham
-    <td>Jul 30, 2006
-    <td>1:01.56
+    <td>Jack Conger (FV)
+    <td>Jul 03, 2010
+    <td>58.26
     <td>15-18 100m Back
     <td>1:06.28
     <td>Jul 23, 2011
@@ -116,9 +116,9 @@
     <td>22
   <tr>
     <td>23
-    <td>Julio Zarate
-    <td>Jul 31, 1994
-    <td>32.90
+    <td>Devin Truong (RV)
+    <td>Jul 23, 2011
+    <td>30.93
     <td>11-12 50m Back
     <td>32.03
     <td>Jul 14, 2012
@@ -126,9 +126,9 @@
     <td>24
   <tr>
     <td>25
-    <td>J.R. Morillo
-    <td>Jul 01, 1997
-    <td>30.77
+    <td>John Jeang (RV)
+    <td>Jul 23, 2011
+    <td>29.87
     <td>13-14 50m Back
     <td>30.60
     <td>Jun 19, 2004
@@ -136,9 +136,9 @@
     <td>26
   <tr>
     <td>27
-    <td>Jeffrey Lin
-    <td>Jun 26, 2010
-    <td>1:01.57
+    <td>Sean Stewart (WG)
+    <td>Jul 21, 2007
+    <td>1:00.52
     <td>15-18 100m IM
     <td>1:07.23
     <td>Jun 26, 2010
@@ -146,9 +146,9 @@
     <td>28
   <tr>
     <td>29
-    <td>Leo Seen
-    <td>Jul 03, 2012
-    <td>22.18
+    <td>Darius Truong (RV)
+    <td>Jul 23, 2011
+    <td>19.37
     <td>8 & Under 25m Breast
     <td>22.22
     <td>
@@ -156,9 +156,9 @@
     <td>30
   <tr>
     <td>31
-    <td>Jesse Cunningham
-    <td>Jul 29, 2001
-    <td>19.21
+    <td>Aaron Stanton (??)
+    <td>Jul 23, 2011
+    <td>18.37
     <td>9-10 25m Breast
     <td>18.34
     <td>Jul 23, 2011
@@ -166,9 +166,9 @@
     <td>32
   <tr>
     <td>33
-    <td>Jesse Cunningham
-    <td>Jul 19, 2003
-    <td>35.59
+    <td>Devin Truong (RV)
+    <td>Jul 23, 2011
+    <td>34.87
     <td>11-12 50m Breast
     <td>36.03
     <td>Jul 23, 2011
@@ -176,9 +176,9 @@
     <td>34
   <tr>
     <td>35
-    <td>Jesse Cunningham
-    <td>Jun 25, 2005
-    <td>32.43
+    <td>Harrison Gu (RV)
+    <td>Jun 26, 2010
+    <td>32.19
     <td>13-14 50m Breast
     <td>34.67
     <td>Jul 12, 2008
@@ -186,9 +186,9 @@
     <td>36
   <tr>
     <td>37
-    <td>Eric Lin
-    <td>Jul 03, 2012
-    <td>1:09.01
+    <td>Daniel Valez (QO)
+    <td>Jul 27, 2002
+    <td>1:05.46
     <td>15-18 100m Breast
     <td>1:12.16
     <td>Jul 03, 2010
@@ -196,9 +196,9 @@
     <td>38
   <tr>
     <td>39
-    <td>Leo Seen
-    <td>Jul 31, 2012
-    <td>18.42
+    <td>Darius Truong (RV)
+    <td>Jul 23, 2011
+    <td>16.19
     <td>8 & Under 25m Fly
     <td>18.45
     <td>Jun 14, 2008
@@ -206,9 +206,9 @@
     <td>40
   <tr>
     <td>41
-    <td>Mark Bosic
-    <td>Jul 01, 1996
-    <td>16.22
+    <td>Timmy Ellett (TW)
+    <td>Jun 18, 2011
+    <td>15.20
     <td>9-10 25m Fly
     <td>15.63
     <td>Jun 18, 2005
@@ -216,9 +216,9 @@
     <td>42
   <tr>
     <td>43
-    <td>Jesse Cunningham
-    <td>Jul 19, 2003
-    <td>31.55
+    <td>Devin Truong (RV)
+    <td>Jul 23, 2011
+    <td>29.80
     <td>11-12 50m Fly
     <td>31.98
     <td>Jun 23, 2012
@@ -226,9 +226,9 @@
     <td>44
   <tr>
     <td>45
-    <td>Jesse Cunningham
-    <td>Jun 25, 2005
-    <td>28.90
+    <td>Brady Fox (HA)
+    <td>Jul 09, 2005
+    <td>27.55
     <td>13-14 50m Fly
     <td>30.13
     <td>Jul 23, 2011
@@ -236,9 +236,9 @@
     <td>46
   <tr>
     <td>47
-    <td>Julio Zarate
-    <td>Jul 01, 2000
-    <td>27.52
+    <td>Jack Conger (FV)
+    <td>Jul 03, 2010
+    <td>26.40
     <td>15-18 50m Fly
     <td>29.57
     <td>Jun 26, 2010
