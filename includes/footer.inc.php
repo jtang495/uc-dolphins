1f63c67fcddb690c1d136dba7a3cf19770fd0ab7
Yang Yang <y4n9squared@gmail.com>
2013-06-10 17:39:13 -0700

    included reach for the wall link in footer

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -4,7 +4,7 @@
       <p class="pull-right"><a href="//status.heroku.com" target="_blank">Site Status</a> &middot; <a href="#">Back to top</a>
       <p>&copy; 2013 Upper County Dolphins. &middot; 8211 Emory Grove Rd. Gaithersburg MD 20878
       <p>
-        <a href="//www.mcsl.org" target="_blank">Montgomery County Swim League (MCSL)</a> &middot; <a href="//www.pvswim.org" target="_blank">Potomac Valley Swimming (PVS)</a> &middot; <a href="//www.usaswimming.org" target="_blank">USA Swimming</a>
+        <a href="//www.mcsl.org" target="_blank">Montgomery County Swim League (MCSL)</a> &middot; <a href="//www.reachforthewall.com">Reach for the Wall - Washington Post Coverage</a> &middot; <a href="//www.pvswim.org" target="_blank">Potomac Valley Swimming (PVS)</a> &middot; <a href="//www.usaswimming.org" target="_blank">USA Swimming</a>
    </div>
 </div>
 
