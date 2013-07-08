2c3085d00a304628f3f32518d28fd68d8d2eacd3
Yang Yang <y4n9squared@gmail.com>
2013-07-07 22:37:51 -0400

    static update - week4a

--- index.php
+++ index.php
@@ -13,32 +13,8 @@
   <div id="main-carousel" class="carousel slide visible-desktop">
     <div class="carousel-inner">
       <div class="item active">
-        <!-- TODO (yang): add alternate media -->
-        <img src="img/slide-01.jpg" alt="">
-        <div class="container">
-          <div class="carousel-caption">
-            <h1>Registration is Open</h1>
-            <p class="lead">
-              Upper County Dolphins is a Division A swim team. Come join the fun! We are #2 in the top division! Our course number is 351003.
-            </p>
-            <a class="btn btn-large btn-primary" href="https://recweb.montgomerycountymd.gov/Start/Start.asp" target="_blank">Register online</a>
-          </div>
-        </div>
+        <img src="img/slide-01.jpg" alt="carousel-photo">
       </div><!-- /.item -->
-      <div class="item">
-        <!-- TODO (yang): add alternate media -->
-        <img src="img/slide-02.jpg" alt="">
-        <div class="container">
-          <div class="carousel-caption">
-            <h1>2013 Spirit Wear has Arrived!</h1>
-            <p class="lead">
-              This year's team suit is the Nike Foil Skin. Order yours now.
-            <p>
-              <a class="btn btn-large btn-primary" href="team-suits.php">Team Suit</a>
-              <a class="btn btn-large" href="dolphin-shop.php">Spirit Wear</a>
-          </div>
-        </div>
-      </div>
     </div>
     <a class="left carousel-control" href="#main-carousel" data-slide="prev">&lsaquo;</a>
     <a class="right carousel-control" href="#main-carousel" data-slide="next">&rsaquo;</a>
@@ -53,75 +29,59 @@
 
   <div class="container">
     <div class="marketing">
-      <!--
       <div class="alert alert-success">
         <button type="button" class="close" data-dismiss="alert">&times;</button>
-        <strong>Upper County beats Tilden Woods for a second straight year! Final Score: 438 - 353. </strong>
+        <strong>Upper County beats Stonegate for chance at division title!</strong>
         Monday's practice will be a play day!
       </div>
-      -->
       <div class="row-fluid">
 
         <!-- Main column -->
         <div class="span4">
           <h2 class="featurette-heading">Announcements</h2>
     		  <hr class="featurette-divider">
+          <h3>A Relay Carnival <span class="label label-warning">NEW</span></h3>
+		      <p class="lead">
+            Sunday, July 7, 2013 <br>
+            <strong>Final Score:</strong>
+            <font color="green">UC 178</font> (2nd place)
+          <p>
+            <a class="btn btn-primary" href="pdf/relay-a-results.pdf" target="_blank">Results <i class="icon-download icon-white"></i></a>
+          <hr class="featurette-divider">
+          <h3>A Meet vs. Stonegate <span class="label label-warning">NEW</span></h3>
+			    <p class="lead">
+            Saturday, July 6, 2013<br>
+            <strong>Final Score: </strong><font color="green">UC 421.5</font> - <font color="red">370.5 SG</font>
+          <p>  
+            <a class="btn" href="pdf/sg-program.pdf" target="_blank">Program <i class="icon-download"></i></a>
+            <a class="btn btn-primary" href="pdf/sg-results.pdf" target="_blank">Results <i class="icon-download icon-white"></i></a>
+		      <hr class="featurette-divider">
           <h3>B Meet vs. Mill Creeke Town</h3>
 		      <p class="lead">  
             Wednesday, July 3, 2013<br>
-            <strong>Final Score: </strong>UC 380 - 455 MCT
+            <strong>Final Score: </strong><font color="red">UC 380</font> - <font color="green">455 MCT</font>
 		      <p>
             <a class="btn btn-primary" href="pdf/mct-results.pdf" target="_blank">Results <i class="icon-download icon-white"></i></a>
           <hr class="featurette-divider">
-          <h3>A Meet vs. Darnestown</h3>
-			    <p class="lead">  
-            Saturday, June 29, 2013<br>
-            <strong>Final Score: </strong> UC 512 - 280 DT
-			    <p>
-            <a class="btn btn-primary" href="pdf/dt-results.pdf" target="_blank">Results <i class="icon-download icon-white"></i></a>
-          <hr class="featurette-divider">
-          <h3>B Meet vs. Olney Mill</h3>
-			    <p class="lead">
-            Wednesday, June 26, 2013<br>
-            <strong>Final Score: </strong> UC 433.5 - 273.5 OM
-			    <p>
-            <a class="btn btn-primary" href="pdf/om-results.pdf" target="_blank">Results <i class="icon-download icon-white"></i></a>
-          <hr class="featurette-divider">
-          <h3>A Meet vs. Tilden Woods</h3>
-			    <p class="lead">
-            Saturday June 22, 2013<br>
-            <strong>Final Score: </strong>UC 438 - 353 TW
-			    <p>
-			      <a class="btn btn-primary" href="pdf/tw-results.pdf" target="_blank">Results <i class="icon-download icon-white"></i></a>
         </div><!-- /.span4 -->
 
         <div class="span5">
           <h2 class="featurette-heading">Upcoming Events</h2>
           <hr class="featurette-divider">
-          <h3>Pot Luck & Pep Rally <span class="label label-warning">NEW</span></h3>
-			    <p class="lead">
-            Friday, July 5, 2013 from 6:00-8:00 pm<br>
-            <a class="btn btn-info" href="pdf/family-potluck-info.pdf" target="_blank">More Details <i class="icon-arrow-right icon-white"></i></a>
-            <a class="btn btn-primary" href="http://www.signupgenius.com/go/10C0D4DABA62B0-social2" target="_blank">Volunteer Sign-up <i class="icon-pencil icon-white"></i></a>
+          <h3>B Meet vs. Hallowell <span class="label label-warning">NEW</span></h3>
+		      <p class="lead">  
+            Wednesday, July 10, 2013<br>
+            Swimmer sign-up closes on Monday at 5:00 pm.
+          <p>
+            <a class="btn" href="//http://www.signupgenius.com/go/10C0D4DABA62B0-bmeetvolunteers2" target="_blank">Volunteer Sign-up <i class="icon-pencil"></i></a>
+            <a class="btn btn-primary" href="//www.SignUpGenius.com/go/10C0D4DABA62B0-bmeetswimmer8/4929" target="_blank">Swimmer Sign-up <i class="icon-pencil icon-white"></i></a>
           <hr class="featurette-divider">
-          <h3>A Meet vs. Stonegate <span class="label label-warning">NEW</span></h3>
-			      <p class="lead">
-            Saturday, July 6, 2013<br>
-            <strong>Where: </strong>Upper County Outdoor Pool<br>
-            <strong>Warm-ups: </strong>7:30 am<br>
-		        <strong>Meet starts: </strong>9:00 am
-		        <hr class="featurette-divider">
-            <h3>A Relay Carnival <span class="label label-warning">NEW</span></h3>
-		        <p class="lead">
-              Sunday, July 7, 2013 <br>
-            <strong>Where: </strong>Stonegate Outdoor Pool<br>
-		        <hr class="featurette-divider">
-            <h3>B Relay Carnival <span class="label label-warning">NEW</span></h3>
-		        <p class="lead">
-              Sunday, July 14, 2013<br>
-              Please sign-up with just a name.<br>
-            <p>
-              <a class="btn btn-primary" href="http://www.signupgenius.com/go/10C0D4DABA62B0-brelay2" target="_blank">Sign-up <i class="icon-pencil"></i></a>
+          <h3>B Relay Carnival</h3>
+          <p class="lead">
+            Sunday, July 14, 2013<br>
+            Please sign-up with just a name.<br>
+          <p>
+            <a class="btn btn-primary" href="//www.signupgenius.com/go/10C0D4DABA62B0-brelay2" target="_blank">Swimmer Sign-up <i class="icon-pencil icon-white"></i></a>
         </div><!-- /.span4 -->
 
         <!-- Sidebar -->
